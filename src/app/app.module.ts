cbb3b87a3b3fc2ba8066d0caca33bcdde04cc60f
hami-r <mynameissaitama54@gmail.com>
2022-11-25 14:20:08 +0530

    add page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCartComponent } from './add-cart/add-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCartComponent
   ],
   imports: [
     BrowserModule,
